1e9d228f2ea215889d6fd54b5559275a563214e7
zairiines12 <zairiines12@gmail.com>
2024-08-01 11:41:10 +0100

    delete validation yaml file

--- validate_yaml.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-with open('docker-compose.yml', 'r') as file:
-    try:
-        yaml.safe_load(file)
-        print("YAML is valid")
-    except yaml.YAMLError as exc:
-        print("YAML is invalid", exc)
